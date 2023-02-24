bbe47054e82e569a9c270c3e954328d7646aa393
Romeolive <livshitsroman225@gmail.com>
2023-02-24 22:28:55 +0800

    Retro

--- app/src/main/java/com/example/speakingai/MainActivity.java
+++ app/src/main/java/com/example/speakingai/MainActivity.java
@@ -11,6 +11,7 @@ import com.chaquo.python.android.AndroidPlatform;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
